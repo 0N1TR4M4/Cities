af47bca8b154d8df41fb8e3560d7caa11ce8555d
PC03-204 <PC03-204@DESKTOP-V4KD07J>
2022-03-15 10:35:51 +0200

    Project

---
+++ Cities/View/CitiesVIew.cs
@@ -0,0 +1,33 @@
+﻿using Cities.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cities.View
+{
+    public partial class CitiesVIew : Form
+    {
+        CitiesController citiesController = new CitiesController();
+
+        public CitiesVIew()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvCities.DataSource = citiesController.GetAll();
+        }
+    }
+}
